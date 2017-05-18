3f8a7384bf3ddbf3a6957f8e54fd41f8b1cf762a
Dana Kulp <dkulp23@gmail.com>
2017-05-18 12:47:43 -0700

    Scaffolding portfolio project using webpack, babel and mithril.

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'bin')
+  },
+  module: {
+    loaders: [{
+      test: '/\.js$/',
+      exclude: '/node_modules/',
+      loader: 'babel-loader'
+    }]
+  }
+};
