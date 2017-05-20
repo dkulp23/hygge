89d19b8ebd36b78f0eee78a20b7782f0d7b1eaef
Dana Kulp <dkulp23@gmail.com>
2017-05-19 18:21:48 -0700

    Material libraries hooked up. Cards rendering to DOM.

--- src/views/RepoList.js
+++ src/views/RepoList.js
@@ -2,27 +2,23 @@
 
 var m = require('mithril');
 var Repos = require('../models/Repos.js');
-var mcm = require('material-components-mithril');
+import {card, cardBlock, cardTitle, cardSubtitle, cardAction, cardMediaItem} from 'material-components-mithril';
 
 module.exports = {
   oninit: Repos.loadList,
   view: function() {
-    return m(mcm.button, 'Submit');
-    //   m.component(list, {
-    //     header: {
-    //       title: 'Github Repos'
-    //     },
-    //     tiles: [
-    //       m.component(listTile, {
-    //         title: 'I am a list tile',
-    //         subtitle: 'Does this work?'
-    //       })
-    //     ]
-        // Repos.list.map( function(repo) {
-        //   return m(listTile, {
-        //     title: repo.name,
-        //     subtitle: repo.id
-        //   })
-        // })
+    return Repos.list.map(function(repo) {
+      return m(card, [
+        m(cardBlock, {type: "primary"}, [
+          m(cardTitle, {large: true}, repo.name),
+          m(cardSubtitle, repo.id)
+        ]),
+        m(cardBlock, repo.description),
+        m(cardBlock, {type: "actions"}, [
+          m(cardAction, repo.html_url),
+          m(cardAction, repo.clone_url)
+        ])
+      ])
+    })
   }
 };
